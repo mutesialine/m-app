118ddd7bbe36b606ba5b83f8fc664ca057faab37
mutesialine <ktesialine@gmail.com>
2023-07-19 23:46:36 +0200

    test app

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View className="h-screen w-full  bg-green-700 flex justify-center  items-center">
       <Text className="text-white">Open up App.js to start working on your app!</Text>
+      <View className="text-2xl bg-red"> chelooo </View>
     </View>
   );
 }
