946c24b6f7df7a806ac052d7f4aa47ac5db6c5c0
mutesialine <ktesialine@gmail.com>
2023-07-21 17:25:05 +0200

    use of button, textInput components

--- App.js
+++ App.js
@@ -1,13 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { useState } from "react";
+import { Button, Text, TextInput, View } from "react-native";
 
 export default function App() {
+  const [name, setName] = useState();
+  const handleName = () => {
+    setName("Mutesi");
+  };
   return (
-    <View className="h-screen w-full  bg-green-700 flex justify-center  items-center">
-      <Text className="text-white">Open up App.js to start working on your app!</Text>
-      <View className="text-2xl bg-red"> chelooo </View>
+    <View className="h-screen w-full  space-y-4 flex justify-center  items-center">
+      <Text className="p-3 bg-yellow-500">Hello, my name is {name}</Text>
+      <View className="p-4 bg-green-500">
+        <Button title="change Name" onPress={handleName} />
+      </View>
+      <View className="space-y-4">
+        <Text>Enter your name:</Text>
+        <TextInput className="p-4 border" placeholder="eg John" onChangeText={(value)=>setName(value)} multiline/>
+        <Text>Name is {name}</Text>
+      </View>
     </View>
   );
 }
-
-
